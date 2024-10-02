24af2ca9cf313f848db9c36e2705db0d987542aa
sannunez <sftwr.gsantosnunes210@gmail.com>
2024-10-02 16:27:33 -0300

    start contact

--- PortfolioSannunez/src/pages/Contact.jsx
+++ PortfolioSannunez/src/pages/Contact.jsx
@@ -1,6 +1,14 @@
 export default function Contact() {
     return ( 
     <>
-        <h1>Contact Page</h1>
+        
+        <body className="gap-10 max-w-[1024px] h-[85vh] my-[30px] mx-[auto] flex justify-center items-center">
+            <div className='w-[50%] h-[70%] bg-black'>
+
+            </div>
+            <div className='w-[50%] h-[70%] bg-black'>
+
+            </div>
+        </body>
     </> );
 }
