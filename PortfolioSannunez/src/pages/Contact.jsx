3be945faf0333c196bd6231d48d0d5f438f813ba
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-02 21:35:59 -0300

    blabla

--- PortfolioSannunez/src/pages/Contact.jsx
+++ PortfolioSannunez/src/pages/Contact.jsx
@@ -2,13 +2,28 @@ export default function Contact() {
     return ( 
     <>
         
-        <body className="gap-10 max-w-[1024px] h-[85vh] my-[30px] mx-[auto] flex justify-center items-center">
-            <div className='w-[50%] h-[70%] bg-black'>
-
+        <body className="gap-10 max-w-[1024px] h-[100vh] my-[30px] mx-[auto] flex justify-center items-center">
+            <div className='w-[50%] h-[70%] bg-[#D9D9D9]'>
+                <div className="flex flex-col items-center justify-center w-[100%] h-[100%] gap-5">
+                    <div className="flex gap-2 items-center justify-start  w-[70%]">
+                        <img src="../../src/assets/images/github.png" alt="GitHub" />
+                        <p className="font-bold">sannunez</p>
+                    </div>
+                    <div className="flex gap-2 items-center justify-start w-[70%]">
+                        <img src="../../src/assets/images/instagram.png" alt="Instagram" />
+                        <p className="font-bold">sannunez.sz</p>
+                    </div>
+                    <div className="flex gap-2 items-center justify-start">
+                        <img src="../../src/assets/images/gmail.png" alt="Gmail" />
+                        <p className="font-bold">sftwr.gsantosnunes@gmail.com</p>
+                    </div>
+                </div>
             </div>
-            <div className='w-[50%] h-[70%] bg-black'>
+            <div className='w-[50%] h-[70%] bg-[#D9D9D9] rounded-[15px]'>
 
             </div>
         </body>
     </> );
 }
+
+// https://www.youtube.com/watch?v=Zbg1BHOVzRg
